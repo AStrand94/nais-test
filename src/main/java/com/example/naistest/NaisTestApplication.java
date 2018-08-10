1ea214ed2ce6325822eddbf6d3a0f88b3c07dcab
Andreas Strand <andreas.strand@nav.no>
2018-08-10 10:58:23 +0200

    Initial commit

---
+++ src/main/java/com/example/naistest/NaisTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.naistest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaisTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaisTestApplication.class, args);
+    }
+}
