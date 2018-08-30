4ef9f8437e640b559b71bfa2646dfc48bce225bf
Andreas Strand <andreas.strand@nav.no>
2018-08-30 15:55:07 +0200

    proxy fix testing

--- frontend/server.js
+++ frontend/server.js
@@ -7,7 +7,7 @@ server.use(express.static(path.join(__dirname, './build')));
 
 server.get('/nais-test-frontend/health', (req, res) => res.sendStatus(200));
 
-server.use(proxy('/api', {target: 'https://nais-test'}));
+server.use(proxy('/api', {target: 'http://nais-test'}));
 
 server.listen(3000, '0.0.0.0', () => {
     console.log('Started')
